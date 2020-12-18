265c774457175f7d1a24b5540ecc1a3eb3a629f6
AlbertoGarciaP <alberto.garcia@wizeline.com>
2020-12-18 12:55:48 -0600

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Alberto Garcia Paul on 18/12/20.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
